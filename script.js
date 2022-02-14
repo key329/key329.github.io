051f5867cf71d38744f5cff25052041e226be0b2
KEY329 <81442512+key329@users.noreply.github.com>
2022-02-14 16:44:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function change()
+   $( ".change" ).on("click", function() {
+			if( $( "body" ).hasClass( "dark" )) {
+				$( "body" ).removeClass( "dark" );
+				$( ".change" ).text( "OFF" );
+			} else {
+				$( "body" ).addClass( "dark" );
+				$( ".change" ).text( "ON" );
+			}
+		});
